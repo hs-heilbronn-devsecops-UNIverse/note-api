8f1463f8d1d98a7c0375d60fb6621a29b1725ef8
Phuong Anh Duong <dhaphuonganh@outlook.com>
2024-12-30 15:46:16 +0100

    test logging

--- note_api/main.py
+++ note_api/main.py
@@ -18,6 +18,7 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import SimpleSpanProcessor
 from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.instrumentation.logging import LoggingInstrumentor
 
 app = FastAPI()
 
@@ -30,6 +31,9 @@ if "pytest" not in sys.modules and os.getenv("ENV") != "test":
     cloud_trace_exporter = CloudTraceSpanExporter()
     span_processor = SimpleSpanProcessor(cloud_trace_exporter)
     tracer_provider.add_span_processor(span_processor)
+
+    # Logging Instrumentation
+    LoggingInstrumentor().instrument()
 
 # Verify the FastAPI app is instrumented to automatically trace all requests
 FastAPIInstrumentor.instrument_app(app)
@@ -62,6 +66,8 @@ def get_notes(backend: Annotated[Backend, Depends(get_backend)]) -> List[Note]:
     # Add custom span
     tracer = get_tracer(__name__)
     with tracer.start_as_current_span("get_notes_operation"):
+
+        logger.info("OpenTelemetry log example with trace_id: %s", tracer.get_current_span().get_span_context().trace_id)
 
         keys = backend.keys()
 
